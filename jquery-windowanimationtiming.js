7f6994c816d74ff05b1328a36b1f564f7e0c5065
Jacob Buck <buck.jacob@gmail.com>
2012-11-12 17:14:50 +1300

    Initial commit.

---
+++ jquery-windowanimationtiming.js
@@ -0,0 +1,31 @@
+/*!
+ * jQuery WindowAnimationTiming - Copyright (c) 2012 Jacob Buck
+ * https://github.com/jacobbuck/jQuery-WindowAnimationTiming
+ * Licensed under the terms of the MIT license.
+ */
+(function ( window, jQuery ) {
+	var timerId,
+		requestAnimationFrame = window.requestAnimationFrame,
+    	cancelAnimationFrame  = window.cancelAnimationFrame,
+    	vendors = [ 'ms', 'moz', 'webkit', 'o' ],
+    	i = 0;
+	for ( ; i < vendors.length && ! requestAnimationFrame; i++ ) {
+		window.requestAnimationFrame = window[vendors[i] + 'RequestAnimationFrame'];
+		window.cancelAnimationFrame  = window[vendors[i] + 'CancelAnimationFrame'] 
+		                            || window[vendors[i] + 'CancelRequestAnimationFrame'];
+	}
+    if ( requestAnimationFrame && cancelAnimationFrame ) {
+		function loop () {
+			jQuery.fx.tick();
+			timerId = requestAnimationFrame( loop );
+		};
+		jQuery.fx.timer = function( timer ) {
+			if ( timer() && jQuery.timers.push( timer ) && ! timerId )
+				timerId = requestAnimationFrame( loop );
+		};
+		jQuery.fx.stop = function() {
+			cancelAnimationFrame( timerId );
+			timerId = 0;
+		};
+	}
+})( window, jQuery );
